2dc2d69137a96cb2e23f7d16d3e81a82d3d30483
Shrey Baxi <coder.shrey@gmail.com>
2024-11-23 22:16:45 +0530

    Dockerize services

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+const mongoHost = process.env.MONGODB_URL || '127.0.0.1';
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/cinema', { useNewUrlParser: true })
-    .catch(e => {
-        console.error('Connection error', e.message)
-    })
+  .connect(`mongodb://${mongoHost}:27017/cinema`, { useNewUrlParser: true })
+  .catch((e) => {
+    console.error('Connection error', e.message);
+  });
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
-module.exports = db
+module.exports = db;
