4dcbb6ce336fe8d8f0001ec0331669db1a29ac2e
Shrey Baxi <coder.shrey@gmail.com>
2024-11-24 01:15:38 +0530

    Update backend app dependencies and code

--- server/db/index.js
+++ server/db/index.js
@@ -2,16 +2,9 @@ const mongoose = require('mongoose');
 
 const mongoHost = process.env.MONGODB_URL || '127.0.0.1';
 
-mongoose
-  .connect(`mongodb://${mongoHost}:27017/cinema`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    reconnectTries: 50,
-    reconnectInterval: 10000,
-  })
-  .catch((e) => {
-    console.error('Connection error', e.message);
-  });
+mongoose.connect(`mongodb://${mongoHost}:27017/cinema`).catch((e) => {
+  console.error('Connection error', e.message);
+});
 
 const db = mongoose.connection;
 
